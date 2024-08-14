f71a0c1b0fa5b26afec13f3d11ac3630dcb67fad
bellaxscobie <bellaxscobie@gmail.com>
2024-08-14 21:16:36 +0000

    Project completed

--- src/app.js
+++ src/app.js
@@ -4,27 +4,27 @@ import "./style.css";
 
 window.onload = () => {
   //write your code here
-  document.querySelector('#excuse').innerHTML = generateExcuse();
-};
-{
-  console.log('Hello Rigo from the console! '+randomNumber);
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
 
 let generateExcuse = () => {
-
-  let pronoun = ['My', 'Your', 'A'];
-  let subject = ['grandma', 'alligator', 'ghost', 'leprechan'];
-  let action = ['ate my', 'stole my', 'danced on my'];
-  let possession = ['homework', 'head', 'pinky toe', 'dog'];
+  let pronoun = ["My", "Your", "A"];
+  let subject = ["grandma", "alligator", "ghost", "leprechan"];
+  let action = ["ate my", "stole my", "danced on my"];
+  let possession = ["homework", "head", "pinky toe", "dog"];
 
   let proIndex = Math.floor(Math.random() * pronoun.length);
   let subIndex = Math.floor(Math.random() * subject.length);
   let actIndex = Math.floor(Math.random() * action.length);
   let posIndex = Math.floor(Math.random() * possession.length);
 
-  return 
-    pronoun[proIndex] + ' ' +
-    subject[subIndex] + ' ' +
-    action[actIndex] + ' ' +
-    possession[posIndex] + '' +;
+  return (
+    pronoun[proIndex] +
+    " " +
+    subject[subIndex] +
+    " " +
+    action[actIndex] +
+    " " +
+    possession[posIndex]
+  );
 };
